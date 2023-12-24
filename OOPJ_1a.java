28954f4ae61349f2c7cf25710f5f56257616fcff
Tanishq Raj <tanishqraj306@gmail.com>
2023-12-24 12:18:13 +0530

    1b done

--- OOPJ_1a.java
+++ OOPJ_1a.java
@@ -2,6 +2,6 @@ public class OOPJ_1a {
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);   
-    }
+        }
     }
 }
---
+++ OOPJ_1b.java
@@ -0,0 +1,9 @@
+public class OOPJ_1b {
+    public static void main(String[] args) {
+        System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
+        System.out.println(Integer.parseInt(args[0])-Integer.parseInt(args[1]));
+        System.out.println(Integer.parseInt(args[0])*Integer.parseInt(args[1]));
+        System.out.println(Integer.parseInt(args[0])/Integer.parseInt(args[1]));
+    }
+}
+
