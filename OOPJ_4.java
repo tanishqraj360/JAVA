d17db0a24b2bb6ec6b2f9612c009a9eda3095dfa
Tanishq Raj <tanishqraj306@gmail.com>
2024-02-08 21:01:37 +0530

    Added main function in OOPJ_4

--- OOPJ_4.java
+++ OOPJ_4.java
@@ -51,4 +51,24 @@ class MyPoint {
     public double distance() {
         return distance(0, 0);
     }
+
+    public static void main(String[] args) {
+        MyPoint p1 = new MyPoint(3, 5);
+        MyPoint p2 = new MyPoint(7, 8);
+
+        System.out.println("Point 1: " + p1.toString());
+        System.out.println("Point 2: " + p2.toString());
+
+        System.out.println("Distance between Point 1 and (0,0): " + p1.distance());
+        System.out.println("Distance between Point 1 and Point 2: " + p1.distance(p2));
+
+        p1.setXY(1, 2);
+        System.out.println("New Coordinates for Point 1: " + p1.toString());
+
+        int[] coordinates = p2.getXY();
+        System.out.println("Coordinates of Point 2: " + coordinates[0] + " and " + coordinates[1]);
+
+        System.out.println("Distance between Point 1 and (0,0): " + p1.distance());
+        System.out.println("Distance between Point 1 and Point 2: " + p1.distance(p2));
+    }
 }
